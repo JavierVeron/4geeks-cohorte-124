483734f0dd68c6b6c7e70244d377b987df3d2dda
Javier Veron <javier.veron@gmail.com>
2025-12-10 06:48:51 -0300

    Lista de Contactos Parte 2

--- 4geeks-app/src/proyectos/lista-contactos/Contactos.jsx
+++ 4geeks-app/src/proyectos/lista-contactos/Contactos.jsx
@@ -6,7 +6,7 @@ const Contactos = () => {
     const BASEURL = "https://playground.4geeks.com/contact";
     const [contacts, setContacts] = useState([]);
 
-    const obtenerUsuario = () => {
+    const obtenerContactos = () => {
         fetch(BASEURL + "/agendas/" + SLUG)
         .then(response => {                      
             if (response.status == 200) {                                
@@ -33,10 +33,25 @@ const Contactos = () => {
     }
 
     useEffect(() => {
-        if (!obtenerUsuario()) {
+        if (!obtenerContactos()) {
             crearUsuario();
         }
     }, [])
+
+    const eliminarUsuario = (id) => {
+        const confirmar = confirm("Desea Eliminar el Usuario #" + id + "?");
+
+        if (confirmar) {
+            fetch(BASEURL + "/agendas/" + SLUG + "/contacts/" + id, {
+                method:"DELETE"
+            })
+            .then(response => {
+                if (response.status == 204) {
+                    console.log("El Usuario #" + id + " se ha eliminado correctamente!");
+                }
+            });
+        }
+    }
 
     return (
         <div className="container my-5">
@@ -45,20 +60,24 @@ const Contactos = () => {
                     <h2>Contactos</h2>
                     <p className="text-end"><Link to={"/addcontact"} className="btn btn-success"><i className="bi bi-person"></i> Agregar Contacto</Link></p>
                     {contacts.length == 0 ? <p className="my-5 text-center fw-bold">No se encontraron Contactos!</p> : 
-                    <ul className="list-group">
-                        {
+                    <table className="table">
+                        <tbody>
+                            {
                             contacts.map(item => (
-                                <li key={item.id} className="list-group-item">
-                                    <span>
-                                        {item.name}<br />
+                                <tr key={item.id}>
+                                    <td>{item.name}<br />
                                         <i className="bi bi-house"></i> {item.address}<br />
                                         <i className="bi bi-envelope"></i> {item.email}<br />
-                                        <i className="bi bi-pen"></i> {item.phone}
-                                    </span>
-                                </li>
+                                        <i className="bi bi-pen"></i> {item.phone}</td>
+                                    <td className="text-end">
+                                        <i className="bi bi-pencil-square mx-1"></i>
+                                        <i className="bi bi-trash" onClick={() => {eliminarUsuario(item.id)}}></i>
+                                    </td>
+                                </tr>
                             ))
                         }
-                    </ul>}
+                        </tbody>
+                    </table>}
                 </div>
             </div>
         </div>
